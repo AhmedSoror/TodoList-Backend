6fd5ac3dec919040b79900a86020a85af13a7119
AhmedSoror <ahmedsoror438@gmail.com>
2019-06-27 17:11:26 +0200

    mongoose connection

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb+srv://todoListUser:P2mVJZu82JMXGJay@todolist-xwvv6.mongodb.net/TodoList?retryWrites=true&w=majority',
+        //mongodb+srv://todoListUser:P2mVJZu82JMXGJay@todolist-xwvv6.mongodb.net/test?retryWrites=true&w=majority
+        secretOrKey:'verysecretkey'
+}
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express=require('express');
-const app=express();
 const bodyParser=require('body-parser');
 const cors=require('cors');
+const mongoose=require('mongoose');
+const config = require("./config/keys.js");
+
+const app=express();
 const port=4000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+  
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("We are connected to MongoDB"))
+  .catch(err => console.log(err));
 
 app.listen(port,()=>{
     console.log(`Server up and running on port ${port}`);
